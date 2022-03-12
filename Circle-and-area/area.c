ed80ed5d5e6e902b8a8f6a5d218be77b947d34f3
tarokh <tarokhyaghoubi@yahoo.com>
2022-03-12 16:36:21 +0330

    Area of a Circle in C

--- Circle-and-area/area.c
+++ Circle-and-area/area.c
@@ -1,13 +1,20 @@
+/*
+ * This is a simple program for
+ * computing the area of a circle
+ *
+ * */
+
+
 #include <stdio.h>
 #define PI 3.14159
 
 int main()
 {
     double area = 0.0, radius = 0.0;
-    printf("Enter Radius : ");
+    printf("Enter radius : \n");
     scanf("%lf", &radius);
     area = PI * radius * radius;
-    printf("Radius of %lf meters; area is %lf sq. meters \n", radius, area);
+    printf("The area of %lf is : %lf \n", radius, area);
     return 0;
-
 }
+
