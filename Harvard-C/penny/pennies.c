e24858bf94d2962d3b5dc406a3a0327955cae245
tarokh <tarokhyaghoubi@yahoo.com>
2022-03-19 23:17:10 +0330

    Making coins in C program CS50X programming in C

---
+++ Harvard-C/penny/pennies.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#include <cs50.h>
+
+int main(void)
+{
+    float amount = get_float("Dollar amount : ");
+
+    int pennies = round(amount * 100);
+
+    printf("Pennies : %i\n", pennies);
+}
