e7ea32987f0039a7749728dda1fd43a254d21ac0
tarokh <tarokhyaghoubi@yahoo.com>
2022-03-19 23:15:57 +0330

    super mario program CS50X programming in C

---
+++ Harvard-C/mario_program/mario.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    } 
+    while(n < 1);
+
+    // for each row 
+
+    for(int i = 0; i < n; i++)
+    {   
+        // for each column 
+
+        for(int j = 0; j < n; j++)
+        {
+            // print a brick 
+
+            printf("*");
+        }
+        // move to the next row   
+        printf("\n"); 
+    }
+}
