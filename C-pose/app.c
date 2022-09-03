ace90c58536ede1baf051218e91ffad286e99572
Tarokh Yaghoubi <tarokhgit@gmail.com>
2022-09-03 14:00:59 +0430

    reedited the file

--- C-pose/app.c
+++ C-pose/app.c
@@ -2,20 +2,21 @@
 
 int main()
 {
-	float fahr, cel;
+	float cel , fahr;
 	float lower, upper, step;
 
 	lower = 0;
 	upper = 300;
 	step = 20;
 
-	cel = lower;
+	fahr = lower;
 
-	while (cel <= upper) {
+	while (fahr <= upper) {
 
-		fahr = (cel * 1.8) + 32;
-		printf("%3.0f %6.1f\n", cel, fahr);
-		cel = cel + step;
+		cel = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, cel);
+		fahr = fahr + step;
 
 	}
 }
+
