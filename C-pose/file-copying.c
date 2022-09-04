6dc5bc46fc334cc4433145c524e625f4ba207f3e
Tarokh Yaghoubi <tarokhgit@gmail.com>
2022-09-04 17:33:24 +0430

    using EOF and getchar , putcha

---
+++ C-pose/file-copying.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* Copy input to output - first version . */
+
+int main()
+
+{
+	int c;
+
+	c = getchar();
+
+	while (c != EOF) {
+
+		putchar(c);
+		c = getchar();
+
+	}
+
+}
+
