a2f68521cc3389bfc4a92368aba8eec7916a05ff
Tarokh Yaghoubi <tarokhgit@gmail.com>
2022-09-04 13:15:09 +0430

    Symbolic Constants

--- C-pose/cel-to-fahr-for-statement.c
+++ C-pose/cel-to-fahr-for-statement.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
 int main()
 
 {
  	float fahr;
 
-	for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
 
 		printf("%3.0f %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
 
