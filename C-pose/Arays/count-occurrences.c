61cee0735afeccb7c566d65e2a0b7d76a945b438
Tarokh Yaghoubi <tarokhgit@gmail.com>
2022-09-05 13:03:45 +0430

    changes on count occurences

--- C-pose/Arays/count-occurrences.c
+++ C-pose/Arays/count-occurrences.c
@@ -1,41 +1,36 @@
 #include <stdio.h>
+/* count digits, white space, others */
 
 int main()
 {
 	int c, i, nwhite, nother;
 	int ndigit[10];
-
 	nwhite = nother = 0;
-
-	for (i = 0; i < 10; ++i) {
+	for (i = 0; i < 10; ++i)
 		ndigit[i] = 0;
-	}
-
-	while ((c = getchar()) != EOF) {
-
-		if (c > '0' && c <= '9') {
+	while ((c = getchar()) != EOF)
+		
+		if (c >= '0' && c <= '9')	
+		
 			++ndigit[c-'0'];
-		}
-
-		else if (c == ' ' || c == '\n' || c == '\t') {
-
+		
+		else if (c == ' ' || c == '\n' || c == '\t')
+			
 			++nwhite;
-
-		}
-		else {
+		else
+			
 			++nother;
 
-		}
-
-	}
-
-	printf("digits = ");
-	for (i = 0; i < 10; ++i) {
+	
+	printf("digits =");
+	
+	for (i = 0; i < 10; ++i)
 		printf(" %d", ndigit[i]);
 
-	}
+	printf(", white space = %d, other = %d\n", nwhite, nother);
 
-	printf(", white space = %d , other = %d\n", nwhite, nother);
+
+
+	return 0;
 
 }
-
