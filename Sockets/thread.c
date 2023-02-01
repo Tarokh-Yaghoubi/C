af0da176a6d85439afbb3fdcfb4376eb6955cd66
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-02-01 13:01:13 +0330

    started learning about FILES , BUFFERS and Eventually Sockets

---
+++ Sockets/thread.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void* myThread(void* vargp);
+
+int chara;
+
+int main(int argc, char* argv[])
+{
+	int u;
+	pthread_t thread_id
+}
+
+void* myThread(void* vargp)
+{
+	int n;
+	for (;;)
+	{
+		int j;
+	}
+		
+	sleep(1);
+	printf("print tarokh from thread \n");
+	return NULL;
+
+}
