bc89e2732b30e6bc5ad7e638b1d3ae2b7302c762
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-02-21 16:51:18 +0330

    an initialization of the do-while loop in C programming Herbert Shieldt

--- Mister Behnia Projects/herbert_dowhile.c
+++ Mister Behnia Projects/herbert_dowhile.c
@@ -1,1 +1,17 @@
 /*	DO-WHILE IN C PROGRAMMING	*/	
+
+/*
+
+	Unlike for and while loops, which test the loop condition at the top of the loop, the do-while loop
+	checks its condition at the bottom of the loop. This means that a do-while loop always executes at
+	least once. The general form of the do-while loop is :
+
+
+	do {
+		statement;
+	} while (condition);
+
+	the braces are not necessary when only one statement is present .
+	The do-while loop iterates until the condition becomes false .
+
+*/
