d9f3c2c4f0f9ff82fd790497d47a2d0fb63aff5d
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-02-25 21:54:34 +0330

    the grades database , a database of 3 CLASSES and 30 GRADES , two dimensional arrays C programming Herbert Shieldt

--- Mister Behnia Projects/grades_database.c
+++ Mister Behnia Projects/grades_database.c
@@ -1,90 +1,93 @@
 /*
 
-	A Database for students grades 
-	3 Classes and 30 Grades 
-
+	A program to get the grades of 30 students in each 3 CLASSES
+	in C programming , Herbert Shieldt book 
+	Tarokh Yaghoubi - Jacob
 
 */
 
-
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-#include <stdlib.h>
 
 #define CLASSES 3
-#define	GRADES 30
+#define GRADES 30
 
 int grade[CLASSES][GRADES];
 
-void display_grades(void);
-void enter_grades(int g[][GRADES]);
+void display_grades(int g[][GRADES]);
+void enter_grades(void);
 int get_grade(int num);
 
 int main(void)
 {
-	char ch, str[80];
+	// write your code here
+
+	char c, str[80];
 
 	for (;;)
 	{
 		do
 		{
-			printf("[E] Enter grades : \n");
-			printf("[R] Report grades : \n");
-			printf("[Q] Quit : \n");
+			printf("[E] Enter the Grades : \n");
+			printf("[R] Report the Grades : \n");
+			printf("[Q] Quit \n");
 
 			gets(str);
-			ch = toupper(*str);
+			c = toupper(*str);
 
-		} while (ch != 'R' && ch != 'E' && ch != 'Q');
-
-		switch (ch)
-		{
-		case 'R':
-			display_grades();
-			break;
-		case 'E':
-			enter_grades(grade);
-			break;
-		case 'Q':
-			exit(0);
-		}
+			switch (c)
+			{
+			case 'E':
+				enter_grades();
+				break;
+			case 'R':
+				display_grades(grade);
+				break;
+			case 'Q':
+				exit(0);
+			}
+		} while (c != 'E' && c != 'R' && c != 'Q');
 	}
+
 	return 0;
+}
+
+void enter_grades(void)
+{
+	int t, i;
+
+	for  (t = 0; t < CLASSES; t++)
+	{
+		printf("Class # : %d \n", t + 1);
+		for (i = 0; i < GRADES; i++)
+			grade[t][i] = get_grade(i);
+	}
 }
 
 int get_grade(int num)
 {
 	char s[80];
 
-	printf("Enter grade for student : %d \n", num + 1);
+	printf("Enter the grade for student : %d \n", num + 1);
 
 	gets(s);
 
-	return(atoi(s));
+	return (atoi(s));
+
 }
 
-
-void display_grades(void)
+void display_grades(int g[][GRADES])
 {
 	int t, i;
 
-	for (t = 0; t < CLASSES; t++)
+	for ( t = 0; t < CLASSES; t++)
 	{
 		printf("Class # : %d \n", t + 1);
 		for (i = 0; i < GRADES; i++)
-			printf("Class # : %d - Student Grade # : %d \n", i + 1, grade[t][i]);
-	}
-}
-
-void enter_grades(int g[][GRADES])
-{
-	int t, i;
-	for (t = 0; t < CLASSES; t++)
-	{
-		printf("Class # : %d \n", t + 1);
-		for (i = 0; i < GRADES; i++)
-			grade[t][i] = get_grade(i);
+		{
+			printf("The grade of student number  : %d is : %d \n", i + 1, g[t][i]);
+		}
 	}
 }
