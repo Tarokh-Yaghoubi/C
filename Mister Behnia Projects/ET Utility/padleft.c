03b71fb70cabfd27910fd7fcbfe1ffe04cdc4d8a
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-04-05 12:16:00 +0330

    some changes to the file

--- Mister Behnia Projects/ET Utility/padleft.c
+++ Mister Behnia Projects/ET Utility/padleft.c
@@ -4,20 +4,14 @@
 #include <windows.h>
 
 typedef unsigned char   uint8;
-typedef unsigned short  uint16;
-typedef unsigned long   uint32;
-typedef long int 		uint64;
-typedef long long int  	int64;
-typedef signed char    	int8;
-typedef signed short   	int16;
-typedef signed long    	int32;
+
 
 int main()
 {
 
-    uint8 buffer[12] = { 0 };
+    uint8 buffer[30] = { 0 };
     strcpy(buffer, "abcd");
-    padLeft(buffer, 5);
+    padLeft(buffer, 22);
     return 0;
 }
 
@@ -25,15 +19,15 @@ void padLeft(uint8* buffer, const uint8 finalLength)
 {
     int     bufferLen = strlen((const char*)buffer);      /** buffer lenght */
 
-    if (bufferLen >= finalLength)
+    if (bufferLen >= finalLength && finalLength > 22)
         return;
 
     uint8 finalBuf[22 + 1];
     memset(finalBuf, '0', finalLength - bufferLen);
     memcpy(finalBuf + (finalLength - bufferLen), buffer, bufferLen);
 
-
-    finalBuf[finalLength] = '\0'; // This line sets the null-terminator in a wrong place , use funalBuf[finalLength] = '\0'
+    if (finalLength != 22)
+        finalBuf[finalLength] = '\0'; // This line sets the null-terminator in a wrong place , use funalBuf[finalLength] = '\0'
     memcpy(buffer, finalBuf, finalLength);
 
     /* showLog(JUST_INGENICO, __FILE__, __LINE__, TRACE, "", "PAD LEFT");
