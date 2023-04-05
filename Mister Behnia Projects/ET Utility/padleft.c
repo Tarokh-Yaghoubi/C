c321271a5a1ed88e705d6e45cd9abfb25c84d5f1
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-04-05 11:06:43 +0330

    PadLeft() , found some bugs

--- Mister Behnia Projects/ET Utility/padleft.c
+++ Mister Behnia Projects/ET Utility/padleft.c
@@ -12,33 +12,6 @@ typedef signed char    	int8;
 typedef signed short   	int16;
 typedef signed long    	int32;
 
-
-//void padLeft(uint8* buffer, const uint8 finalLength)
-//{
-//    // int     bufferSize = sizeof(*buffer); /* wrong implement no usage */
-//    int lengh_fix;
-//    int 	bufferLen = strlen((const char*)buffer);      /** buffer lenght */
-//    uint8	finalBuf[22] = "0000000000000000000000";
-//    
-//    if (finalLength >= 22) // Babak Hajizadeh 
-//    {
-//        //lengh_fix = 21;
-//        //padLeft(buffer, lengh_fix); // const finalLengh could not be modified recursively
-//
-//        /* buffer length mismatch */
-//        /* TODO: showlog propper error */
-//        return;
-//    }
-//    finalBuf[finalLength] = 0; //gpt: this line sets the null terminator at a wrong index , it shoud be set at finalBuf[funalLength - 1]
-//    
-//
-//
-//    memmove(&finalBuf[finalLength - bufferLen], buffer, bufferLen); 
-//    memcpy(buffer, finalBuf, finalLength + 1/* +1 babak*/);
-//    /* Babak: in order to explicitly null - terminate the outter buffer
-//    for more safety however buffers are often nullt-erminated */
-//}
-
 int main()
 {
 
@@ -60,8 +33,7 @@ void padLeft(uint8* buffer, const uint8 finalLength)
     memcpy(finalBuf + (finalLength - bufferLen), buffer, bufferLen);
 
 
-    finalBuf[finalLength] = '\0'; // gpt : this line sets the null-terminator in a wrong place , use funalBuf[finalLKength] = 0
-    // memmove(&finalBuf[finalLength - bufferLen], buffer, bufferLen);
+    finalBuf[finalLength] = '\0'; // This line sets the null-terminator in a wrong place , use funalBuf[finalLength] = '\0'
     memcpy(buffer, finalBuf, finalLength);
 
     /* showLog(JUST_INGENICO, __FILE__, __LINE__, TRACE, "", "PAD LEFT");
