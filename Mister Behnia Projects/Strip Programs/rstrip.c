d2ab848c307aad199b5eb5c17088b25d45aa6053
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-04-18 10:00:12 +0330

    Remove White-Space from right-side of a Character Array , C programming

--- Mister Behnia Projects/Strip Programs/rstrip.c
+++ Mister Behnia Projects/Strip Programs/rstrip.c
@@ -4,10 +4,24 @@
 
 char *rstripstring(char* s);
 
+/*
+    
+    Tarokh (Jacob) Yaghoubi , A program to remove white-space 
+    from right-side of a Character Array
+
+*/
+
 int main(int argc, char *argv[])
 {
     char name[] = "tarokh yaghoubi      ";
-    printf("%s", rstripstring(name));
+    // Before
+    printf("{%s}\n", name);
+
+    // After 
+    printf("{%s}\n", rstripstring(name));
+    
+    // Main Character Array has changed 
+    printf("{%s}", name);
     return 0;
 }
     
