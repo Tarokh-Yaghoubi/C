2220fe13c9efacee6cdc19f20ad7d30914137b88
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-04-18 11:32:09 +0330

    Remove Special Characters from Both-Sides of a Character Array

---
+++ Mister Behnia Projects/Strip Programs/remove_special_characters.c
@@ -0,0 +1,68 @@
+/*
+
+	Tarokh (Jacob) Yaghoubi , A Program to remove special Characters from 
+	both-sides of a character array .
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+char* index_last(char* ptr);
+int index_beginning(char* ptr);
+
+int main()
+{
+	char name[] = "@# tarokh is a good boy  @@#!@##$";
+	char* ptr;
+	ptr = &name[0];
+	int len = strlen(name);
+	int first_char = index_beginning(name);
+	memmove(&name[0], &name[first_char], (len - first_char));
+	
+	// name[len - first_char] = '\0';
+
+	printf("{%s}\n", index_last(name));
+	printf("{%s} \n", ptr);
+	printf("{%s} \n", name);
+	
+
+	return 0;
+}
+
+int index_beginning(char* ptr)
+{
+	// Start from very first
+
+	int i = 0;
+	while ((*ptr < 65 || *ptr > 91) && (*ptr < 97 || *ptr > 122))
+	{
+		
+		i++;	
+		ptr++;
+		
+	}
+	
+	return i;
+}
+
+char* index_last(char* ptr)
+{
+	// Start from the very last
+
+	size_t size;
+	char* end;
+
+	size = strlen(ptr);
+
+	if (!size)
+		return ptr;
+
+	end = (ptr + size) - 1;
+	while (end >= ptr && ((*end < 65 || *end > 91) && (*end < 97 || *end > 122)))
+		end--;
+
+	*(end + 1) = '\0';
+
+	return ptr;
+}
