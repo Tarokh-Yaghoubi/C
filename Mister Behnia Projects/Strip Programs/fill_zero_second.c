1122b8f38b11af7db03dd6078e4423715d29f1f9
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-04-19 15:33:11 +0330

    Second version of Fill Zero program , C programming

--- Mister Behnia Projects/Strip Programs/fill_zero_second.c
+++ Mister Behnia Projects/Strip Programs/fill_zero_second.c
@@ -4,6 +4,7 @@
 	second version . (test)
 
 */
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
@@ -18,18 +19,12 @@ int main()
 	int new_len = 0;
 	char number[12] = "12";
 	char result[13] = "000000000000";
-	
-	//for (int i = 0; i < sizeof(result) / sizeof(result[0]); i++)
-	//	result[i] = '0';
 
 	len = strlen(&number[0]);
 	new_len = max - len;
 	strcpy(&result[new_len], &number[0]);	
 	result[12] = '\0';
 
-	//if (sizeof(result) / sizeof(result[0]) > 12)
-	//	return -1;
-	
 	for (int i = 0; i < sizeof(result) / sizeof(result[0]); i++)
 		printf("%c", result[i]);
 	
