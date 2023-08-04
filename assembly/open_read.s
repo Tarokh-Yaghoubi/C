e3ddbc0d1e43b708f72ba41d1f7c65a669620af5
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-08-04 20:22:50 +0330

    translated my C program that opens a file and reads its data into Assembly

---
+++ assembly/open_read.s
@@ -0,0 +1,89 @@
+	.file	"main.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata
+.LC0:
+	.string	"r"
+.LC1:
+	.string	"example.txt"
+.LC2:
+	.string	"Error opening the file"
+.LC3:
+	.string	"Content of the file:\n%s\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	add	rsp, -128
+	mov	rax, QWORD PTR fs:40
+	mov	QWORD PTR -8[rbp], rax
+	xor	eax, eax
+	lea	rax, .LC0[rip]
+	mov	rsi, rax
+	lea	rax, .LC1[rip]
+	mov	rdi, rax
+	call	fopen@PLT
+	mov	QWORD PTR -120[rbp], rax
+	cmp	QWORD PTR -120[rbp], 0
+	jne	.L2
+	lea	rax, .LC2[rip]
+	mov	rdi, rax
+	call	perror@PLT
+	mov	eax, 1
+	jmp	.L4
+.L2:
+	mov	rdx, QWORD PTR -120[rbp]
+	lea	rax, -112[rbp]
+	mov	rcx, rdx
+	mov	edx, 100
+	mov	esi, 1
+	mov	rdi, rax
+	call	fread@PLT
+	mov	rax, QWORD PTR -120[rbp]
+	mov	rdi, rax
+	call	fclose@PLT
+	lea	rax, -112[rbp]
+	mov	rsi, rax
+	lea	rax, .LC3[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	mov	eax, 0
+.L4:
+	mov	rdx, QWORD PTR -8[rbp]
+	sub	rdx, QWORD PTR fs:40
+	je	.L5
+	call	__stack_chk_fail@PLT
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
---
+++ assembly/open_read_att.s
@@ -0,0 +1,88 @@
+	.file	"main.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"r"
+.LC1:
+	.string	"example.txt"
+.LC2:
+	.string	"Error opening the file"
+.LC3:
+	.string	"Content of the file:\n%s\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	addq	$-128, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	call	fopen@PLT
+	movq	%rax, -120(%rbp)
+	cmpq	$0, -120(%rbp)
+	jne	.L2
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdi
+	call	perror@PLT
+	movl	$1, %eax
+	jmp	.L4
+.L2:
+	movq	-120(%rbp), %rdx
+	leaq	-112(%rbp), %rax
+	movq	%rdx, %rcx
+	movl	$100, %edx
+	movl	$1, %esi
+	movq	%rax, %rdi
+	call	fread@PLT
+	movq	-120(%rbp), %rax
+	movq	%rax, %rdi
+	call	fclose@PLT
+	leaq	-112(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+.L4:
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L5
+	call	__stack_chk_fail@PLT
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
