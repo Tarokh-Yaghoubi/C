039f5bd490dfdc545003188b24a1786fdc66c110
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.LOCAL>
2023-08-08 12:35:36 +0330

    The Linux Programming Interface Header file

---
+++ The Linux Programming Interface/Chapter 3/tlpi_hdr.h
@@ -0,0 +1,31 @@
+/*////////////////////////////////////////////////////*/
+//                                                   //   
+//                                                   //
+//             Author : Tarokh                       //
+//             Most used header files                //
+//                                                   //
+//                                                   //
+/*////////////////////////////////////////////////////*/
+
+
+#ifndef TPLI_HEADER_H
+#define TPLI_HEADER_H
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+
+#include "error_functions.h"    /* Declares our error-handling functions */
+
+typedef enum { FALSE, TRUE } Boolean;
+
+#define min(m,n) ((m) < (n) ? (m) : (n))
+#define max(m,n) ((m) > (n) ? (m) : (n))
+
+#endif
