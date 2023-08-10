25919c5f0bb8f4d7de57ee57ffcb8ebf55b64ebd
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-08-10 19:38:51 +0330

    added some examples and header files

---
+++ The Linux Programming Interface/Chapter 3/getid.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <stdarg.h>
+#include "tlpi_hdr.h"
+
+#include <linux/reboot.h>
+
+
+
+
+int main()
+{
+    size_t sz = 15;
+    __pid_t mypid;
+    /* A portable way to use sembuf structure is this */
+
+    struct sembuf s;
+    s.sem_num = 3;
+    s.sem_op = -1;
+    s.sem_flg = SEM_UNDO;
+    
+
+    /* C99 new syntax for structure initializers */
+    struct sembuf ss = {
+        .sem_num = 3,
+        .sem_op = -1,
+        .sem_flg = SEM_UNDO
+    };
+
+#ifdef WCOREDUMP
+    /* Use WCOREDUMP() macro */
+#endif
+
+
+    mypid = getpid();
+
+    printf("My PID is : %ld\n", (long)mypid);
+    printf("The size of sz is : %zd\n", (long)sz);
+    
+    return 0;
+
+}
--- The Linux Programming Interface/Chapter 3/tlpi_hdr.h
+++ The Linux Programming Interface/Chapter 3/tlpi_hdr.h
@@ -15,6 +15,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "sys/ipc.h"
+#include "sys/sem.h"
 
 #include <unistd.h>
 #include <errno.h>
