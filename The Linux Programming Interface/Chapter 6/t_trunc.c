85227bba50273adf59835bd837b93304a1c311fd
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-10-09 20:14:46 +0330

    truncate system call example | added a line in the beginning of the file for test

--- The Linux Programming Interface/Chapter 6/t_trunc.c
+++ The Linux Programming Interface/Chapter 6/t_trunc.c
@@ -4,20 +4,27 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <fcntl.h>
 
 enum RETVALS {
 
 	SUCCESS,
 	ERRUSAGE,
-	ERRTRUNC
+	ERRTRUNC,
+	ERRFILEOPEN,
+	ERRWRITETOFILE
 };
 
 int
 main(int argc, char* argv[]) {
 
 	off_t size = 524;
+	char* msg = "tarokh is a good C developer";
+	off_t offset;
+	size_t msg_len = 0;
 	int retVal = 0;
+	int fd;
+	fd = 0;
 
 	if (argc != 2 || (strcmp(argv[1], "--help")) == 0)
 	{
@@ -31,6 +38,26 @@ main(int argc, char* argv[]) {
 		perror("truncate");
 		return ERRTRUNC;
 	}
+
+	fd = open(argv[1], O_RDWR);
+	if (fd == -1) {
+		perror("open: could not open the file that has been truncated");
+		close(fd);
+		return ERRFILEOPEN;
+	}
+
+	lseek(fd, 0, SEEK_SET);
+
+	msg_len = strlen(msg);
+
+	ssize_t bytes_written = write(fd, msg, msg_len);
+	if (bytes_written == -1) {
+		perror("write");
+		close(fd);
+		return ERRWRITETOFILE;
+	}
+
+	close(fd);
 
 	return 0;
 
